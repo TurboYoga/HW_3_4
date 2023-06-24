ae6ae3e88e2dab566e606aa21fde11606c583378
TurboYoga <kovin1333@gmail.com>
2023-06-24 22:11:07 +0300

    change str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,13 @@
 def foo(words):
-    'функция увеличения слова'
+    '''функция увеличения слова'''
     return words.upper()
+
+
+def foo2 (word):
+    '''Докстринг'''
+    word_1 = word.split()
+    result = []
+    for a in word_1:
+        result.append(a.capitalize())
+    return ' '.join(result)
+
